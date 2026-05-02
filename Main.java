d87b86fd142bf6dd6d97398ac7d9bcf69f6e3df2
Shirin Tayyebat <shirintayyebat@gmail.com>
2026-05-02 20:47:24 +0330

    extract calculator logic into separate class

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
 
-	int a = 5;
-	int b = 3;
-	System.out.println("addition operation:" + (a + b));
-	System.out.println("subtraction operation:" + (a - b));
+        Calculator calculator = new Calculator();
 
+        System.out.println("addition operation: " + calculator.add(a, b));
+        System.out.println("subtraction operation: " + calculator.subtract(a, b));
     }
 }
